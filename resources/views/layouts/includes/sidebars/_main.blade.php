950dd4df1f0ef4fa8c71e1fe7c0c22fcb446b091
amr <amr_dotnet@ymail.com>
2020-08-04 20:28:36 +0200

    prepared-project

--- resources/views/layouts/includes/sidebars/_main.blade.php
+++ resources/views/layouts/includes/sidebars/_main.blade.php
@@ -1,9 +1,18 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
     <div class="main-menu-content">
       <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-        <li class=" nav-item"><a href="{{route('main.dashboard')}}"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.support_raise_support.main">{{ trans('admin.dashboard') }}</span></a>
+        {{-- dashboard --}}
+        <li class=" nav-item">
+            <a href="{{route('main.dashboard')}}"><i class="la la-home">
+                </i><span class="menu-title" data-i18n="nav.support_raise_support.main">{{ trans('admin.dashboard') }}</span>
+            </a>
         </li>
-        
+        {{-- website --}}
+        <li class=" nav-item">
+            <a href="{{route('main.dashboard')}}"><i class="la la-globe">
+                </i><span class="menu-title" data-i18n="nav.support_raise_support.main">{{ trans('admin.website') }}</span>
+            </a>
+        </li>
 
       </ul>
     </div>
